91752ee02e05fe7eae60e563a84bcbbfed3e9e68
Seiji Sogabe <s.sogabe@gmail.com>
2014-11-11 21:56:09 +0900

    日本語を使用

--- src/main/java/com/buildria/camel/examle/seda/BookDataSet.java
+++ src/main/java/com/buildria/camel/examle/seda/BookDataSet.java
@@ -8,7 +8,7 @@ public class BookDataSet extends DataSetSupport {
     @Override
     protected Object createMessageBody(long messageIndex) {
         String no = String.format("%05d", messageIndex);
-        return  new Book((int) messageIndex, "title: " + no, "author: " + no);
+        return  new Book((int) messageIndex, "タイトル: " + no, "著者: " + no);
     }
 
 }
